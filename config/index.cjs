d5fca78e04003173c2d2a36863cb7149cb24f72d
rechallage <85208463+rechallage@users.noreply.github.com>
2023-04-16 21:54:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,21 +19,21 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '姚宇',
+      name: '小黄宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o5A6A6QUnv0KvbIY_78DI3b5goME',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6aRxrz6DWoKXRlXKb6k-kuOA6ENVnbAUkTskfmnn5JQ',
+      useTemplateId: 'xM0XK7l8Yu6mZhvb55kH288JbZQZEKq1_hmT-pT4oeE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-19',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '姚宇', year: '2000', date: '01-29',
+          type: '*生日', name: '小黄宝贝', year: '2000', date: '01-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '姚宇', year: '2000', date: '02-19',
+          type: '生日', name: '小黄宝贝', year: '2000', date: '02-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
