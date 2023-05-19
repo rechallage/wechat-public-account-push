3079c2ccf8d660024e861dc473e6e805e3d18452
rechallage <85208463+rechallage@users.noreply.github.com>
2023-05-20 01:42:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o5A6A6T50mkeoEwm5KlDcIp5AByU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'UrWYEaRaT8n0iV0_jT_16GLHVK43FXMXCWZ6AR83eXc',
+      useTemplateId: 'lLDcYEZpEBAtQ6P6IJKDGS0dgIYkjdO4mwNnPu4tS8E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-19',
       festivals: [
