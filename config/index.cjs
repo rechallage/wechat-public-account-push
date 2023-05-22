38e5ff7d309c9605d53cba5ed14203d907889ec9
rechallage <85208463+rechallage@users.noreply.github.com>
2023-05-22 17:51:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小黄宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o5A6A6WlQvh3O1RAohR02byTjBfk',
+      id: 'o5A6A6T50mkeoEwm5KlDcIp5AByU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7HqwfvDbvYZMJizgK8NCc8yQHo-DwpBmJlkghCDjD8c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
